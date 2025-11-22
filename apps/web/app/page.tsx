3119b3b22224a532e94651f1691fc49930dc9a91
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-11-22 14:29:08 +0100

    feat: web app tailwind config init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -38,6 +38,10 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
+        <h1 className="text-3xl font-bold underline text-amber-300">
+          Hello world!
+        </h1>
+
         <div className={styles.ctas}>
           <a
             className={styles.primary}
