5524ccb52b22c478d41fcdee55ee2772cf386898
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-11-22 23:00:21 +0100

    feat: tailwind init extension

--- apps/extension/entrypoints/popup/App.tsx
+++ apps/extension/entrypoints/popup/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from '@/assets/react.svg';
 import wxtLogo from '/wxt.svg';
 import './App.css';
+import "@workspace/tailwind-config";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>WXT + React</h1>
+      <h1 className="text-3xl font-bold underline text-amber-300">WXT + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
