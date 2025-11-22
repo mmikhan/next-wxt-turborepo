0f797338098e881a2ed015ea7d5bea353f582334
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-11-22 18:34:30 +0100

    feat: shadcn web app init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
+import { Button } from "@workspace/ui/components/button";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
@@ -67,7 +67,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
+        <Button variant='destructive'>
           Open alert
         </Button>
       </main>
