28ef8e3ceb2edf72ea1ea594917c4d486198b3a0
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-11-24 14:16:18 +0100

    refactor: extension drop wxt path alias
    Causes conflict with CSS import path during dev and build mode. `bunx
    --bun shadcn@latest add alert -p ../../packages/ui/src/components/`
    could be used to add shadcn/ui components through CLI

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -1,4 +1,3 @@
-import { resolve } from "node:path";
 import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
@@ -7,8 +6,4 @@ export default defineConfig({
   srcDir: "src",
   entrypointsDir: "app",
   modules: ["@wxt-dev/module-react"],
-  alias: {
-    // Only needed for shadcn/ui CLI to add components to the internal package
-    "@workspace/ui": resolve(__dirname, "../packages/ui/src"),
-  },
 });
